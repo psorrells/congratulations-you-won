0a260f7061fed7d77a47d41bc05df5e108a893f7
psorrells <pamelan.sorrells@gmail.com>
2022-04-12 18:20:18 -0500

    added fetch to dnd and pexels

--- js/main.js
+++ js/main.js
@@ -15,6 +15,17 @@ function getFetch(){
             document.querySelector('h2').innerText = data.name
             document.querySelector('h3').innerHTML = data.desc[0] + '<br>' + data.desc[1]
 
+            fetch(`https://api.pexels.com/v1/search?query=${data.name}`,{
+              headers: {
+              Authorization: "563492ad6f917000010000018be4c61f98004329b4003a4e20673201"
+                }
+              })
+              .then(res => res.json()) // parse response as JSON
+              .then(data => {
+                document.querySelector('img').src = data.photos[0].src.medium
+              })
+              .catch(err => {console.log(err)})
+
           })
       })
       .catch(err => {
